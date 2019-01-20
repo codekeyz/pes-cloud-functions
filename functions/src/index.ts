ada0dcde6ddc5c9f18eb6c5b95625d925d5fbba4
Hover Software Solutions <ositachima.co@gmail.com>
2019-01-20 07:01:14 +0000

    Added Manager Status Changed Function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,37 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const databaseInstance = admin.firestore();
+
+export const sendManagerStatusChangedNotification = functions.firestore
+  .document('Managers/{managerID}')
+  .onUpdate((snap, event) => {
+    const oldData: any = snap.before.data();
+    const newData: any = snap.after.data();
+    const oldStatus = oldData.isAccountConfirmed;
+    const newStatus = newData.isAccountConfirmed;
+    const token = newData.myToken;
+    const branchName = newData.branchName;
+    let message = '';
+    if (oldStatus !== newStatus && token !== null) {
+      if (newStatus === true) {
+        message = `Your account has been activated, You are the manager for ${branchName}`;
+      } else {
+        message =
+          'Your account has been deactivated, Contact Administrator for more info';
+      }
+
+      const payload = {
+        data: {
+          title: 'Account Status Notification',
+          message: message
+        }
+      };
+
+      return admin.messaging().sendToDevice(token, payload);
+    } else {
+      return false;
+    }
+  });
